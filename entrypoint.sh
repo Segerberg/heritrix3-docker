e465f2d527a755fe7faa1ffb6b6ce71e1e9fad98
Andreas Segerberg <andreassegerberg@gmail.com>
2022-01-24 14:01:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 
+: ${USER:=admin}
+: ${PASSWORD:=admin}
 
-echo "Starting heritrix...."
+# And fire it up:
+/opt/heritrix/bin/heritrix -a admin:admin -b 0.0.0.0 -j /mnt/heritrix-data/jobs
+
