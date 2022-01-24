5126c803373e3f2fe66193e9e8e31458303b2ecb
Andreas Segerberg <andreassegerberg@gmail.com>
2022-01-24 11:46:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-_JOBARGS="-b /"
+# Set up variables, using defaults if unset:
+: ${HERITRIX_USER:=heritrix}
+: ${HERITRIX_PASSWORD:=heritrix}
 
-# set credentials (require both USERNAME and PASSWORD)
-# -a "${USERNAME}:${PASSWORD}"
-if [[ ! -z "$USERNAME" ]] && [[ ! -z "$PASSWORD" ]]; then
-    echo "${USERNAME}:${PASSWORD}" > ${HERITRIX_HOME}/credentials.txt
-    _JOBARGS="$_JOBARGS -a @${HERITRIX_HOME}/credentials.txt"
-elif [[ ! -z "$CREDSFILE" ]]; then
-    _JOBARGS="$_JOBARGS -a @${CREDSFILE}"
-else
-    >&2 echo "No USERNAME and/or PASSWORD environment var set!"
-fi
-
-# check if -r mode (only from version 3.4.0-20210803)
-if [[ ! -z "$JOBNAME" ]]; then
-    >&2 echo "Found JOBNAME envvar, just running job: $JOBNAME"
-    _JOBARGS="$_JOBARGS -r $JOBNAME"
-    if [ ! -f "/opt/heritrix/jobs/$JOBNAME/crawler-beans.cxml" ]; then
-        >&2 echo "Did not find any 'crawler-beans.cxml' for job '$JOBNAME'!"
-    fi
-fi
-
-# run
-exec ${HERITRIX_HOME}/bin/heritrix $_JOBARGS
+# And fire it up:
+./opt/heritrix/bin/heritrix -a $HERITRIX_USER:$HERITRIX_PASSWORD -b 0.0.0.0 -j /jobs
