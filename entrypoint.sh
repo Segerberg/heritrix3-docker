afa57f5945439e0c688362e8736f517a76514e7f
Andreas Segerberg <andreassegerberg@gmail.com>
2022-01-24 11:59:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-# Set up variables, using defaults if unset:
-: ${HERITRIX_USER:=heritrix}
-: ${HERITRIX_PASSWORD:=heritrix}
 
-# And fire it up:
-./opt/heritrix/bin/heritrix -a $HERITRIX_USER:$HERITRIX_PASSWORD -b 0.0.0.0 -j /jobs
+
+echo "Starting heritrix...."
